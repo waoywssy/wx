822a186d575107e01fe005c4a34feb5dcf27f43b
hailongyu <hailongyu@anjuke.com>
2014-09-24 15:30:44 +0800

    fix autoload

--- inc/global.php
+++ inc/global.php
@@ -6,7 +6,7 @@ require_once(APP_PATH.'/inc/global.php');
 require_once(APP_PATH.'/inc/functions.php');
 
 // Add your class dir to include path
-set_include_path(get_include_path().PATH_SEPARATOR.MODEL_PATH);
+set_include_path(MODEL_PATH);
 // You can use this trick to make autoloader look for commonly used "My.class.php" type filenames
 spl_autoload_extensions('.php');
 // Use default autoload implementation
--- wxapi.php
+++ wxapi.php
@@ -5,7 +5,7 @@
 
 require_once('inc/global.php');
 access_control_header();
-require_once(MODEL_PATH."/WechatRecieve.php");
+
 $wechatObj = new WechatRecieve();
 //$wechatObj->valid();
 $wechatObj->responseMsg();
