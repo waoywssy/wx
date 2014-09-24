f2f7751a636f156b91a1b59adb725bdc5a0d0e12
hailongyu <hailongyu@anjuke.com>
2014-09-24 15:35:27 +0800

    use require instead of autoload

--- inc/global.php
+++ inc/global.php
@@ -4,10 +4,3 @@ define(APP_PATH, realpath(dirname(dirname(__FILE__))));
 define(MODEL_PATH, APP_PATH."/model");
 require_once(APP_PATH.'/inc/global.php');
 require_once(APP_PATH.'/inc/functions.php');
-
-// Add your class dir to include path
-set_include_path(MODEL_PATH);
-// You can use this trick to make autoloader look for commonly used "My.class.php" type filenames
-spl_autoload_extensions('.php');
-// Use default autoload implementation
-spl_autoload_register();
--- wxapi.php
+++ wxapi.php
@@ -2,10 +2,9 @@
 /**
  * wechat php test
  */
-
 require_once('inc/global.php');
 access_control_header();
-
+require_once(MODEL_PATH."/WechatRecieve.php");
 $wechatObj = new WechatRecieve();
 //$wechatObj->valid();
 $wechatObj->responseMsg();
